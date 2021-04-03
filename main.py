437c304eee83dfedc5208a28f88f4bd93e9c5a72
akosh567 <akosh3666@gmail.com>
2021-04-03 11:47:54 +0500

    akosh

---
+++ main.py
@@ -0,0 +1,19 @@
+# print ('Hello world!')
+# print ((1 > 2 and (0 < 5 or 0 < 2)) and 0 == 0)
+
+
+
+# first_name = input('Введите ваше имя: ')
+# last_name = input('Введите вашу фамилию: ')
+# age = int(input('Сколько вам лет: '))
+# weight = int(input('Какой у вас вес: '))
+# print(first_name, last_name, ":", end=" ")
+# if age < 30 and 50 <= weight <= 120:
+#     print("Пациент в хорошем состоянии")
+# elif age > 40 and 50 > weight < 120:
+#     print("Пациенту требуется врачебный осмотр")
+# elif age > 30 and 50 > weight < 120:
+#     print("Пациенту требуется заняться собой")
+# else:
+#     print("Такой вариант ещё не разработан")
+
