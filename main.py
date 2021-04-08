ac6537891a20c6fa9bded61651a8df1492bdb806
akosh567 <akosh3666@gmail.com>
2021-04-08 13:36:39 +0500

    py

--- main.py
+++ main.py
@@ -1,27 +1,22 @@
-# print ('Hello world!')
-# print ((1 > 2 and (0 < 5 or 0 < 2)) and 0 == 0)
+import random
 
+number = random.randint(1, 100)
+print(number)
+user_number = None
+count = 0
+max_count = 3
 
-
-# first_name = input('Введите ваше имя: ')
-# last_name = input('Введите вашу фамилию: ')
-# age = int(input('Сколько вам лет: '))
-# weight = int(input('Какой у вас вес: '))
-# print(first_name, last_name, ":", end=" ")
-# if age < 30 and 50 <= weight <= 120:
-#     print("Пациент в хорошем состоянии")
-# elif age > 40 and 50 > weight < 120:
-#     print("Пациенту требуется врачебный осмотр")
-# elif age > 30 and 50 > weight < 120:
-#     print("Пациенту требуется заняться собой")
-# else:
-#     print("Такой вариант ещё не разработан")
-
-print ('shashsahsa')
-print ('aaaaaaa')
-
-print ('Vs studio')
-
-print ('Vs type')
-
-print ('Vs school')
+while number != user_number:
+    count += 1
+    print(f'Popitka N {count}')
+    user_number = int(input('Vedite chislo: '))
+    if count == max_count:
+        print('Vi proigrali:')
+        break
+    if number == user_number:
+        print('Pobeda')
+        break
+    elif number < user_number:
+        print('Vash chislo bolshe zagadonnogo!')
+    else:
+        print('Vash chislo menshe zagadonnogo!')
