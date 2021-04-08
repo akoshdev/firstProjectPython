1c13f3d0c7614df6e526d35c9762e054df272c47
akosh567 <akosh3666@gmail.com>
2021-04-08 15:40:21 +0500

    w

--- main.py
+++ main.py
@@ -1,25 +1,37 @@
+# import random
+#
+# number = random.randint(1, 100)
+# print(number)
+# user_number = None
+# count = 0
+# levels = {1: 10, 2: 5, 3: 3}
+# level = int(input('Viberite uroven slojnosti: '))
+# # print(level)
+# max_count = levels[level]
+#
+# while number != user_number:
+#     count += 1
+#     print(f'Popitka N {count}')
+#     user_number = int(input('Vedite chislo: '))
+#     if count == max_count:
+#         print('Vi proigrali:')
+#         break
+#     if number == user_number:
+#         print('Pobeda')
+#         break
+#     elif number < user_number:
+#         print('Vash chislo bolshe zagadonnogo!')
+#     else:
+#         print('Vash chislo menshe zagadonnogo!')
+
+
 import random
 
-number = random.randint(1, 100)
-print(number)
-user_number = None
-count = 0
-levels = {1: 10, 2: 5, 3: 3}
-level = int(input('Viberite uroven slojnosti: '))
-# print(level)
-max_count = levels[level]
+a = random.randint(1, 200)
+b = random.randint(1, 200)
+c = int(input(f'{a} + {b} = '))
 
-while number != user_number:
-    count += 1
-    print(f'Popitka N {count}')
-    user_number = int(input('Vedite chislo: '))
-    if count == max_count:
-        print('Vi proigrali:')
-        break
-    if number == user_number:
-        print('Pobeda')
-        break
-    elif number < user_number:
-        print('Vash chislo bolshe zagadonnogo!')
-    else:
-        print('Vash chislo menshe zagadonnogo!')
+if c == (a + b):
+    print("Tog'ri:")
+else:
+    print("No tog'ri:")
