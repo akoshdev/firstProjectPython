a22bdaa80cb13f5c3586c9544694d91b99d64b8f
Akbarjon Xatamov <akbarxatamov7545@gmail.com>
2021-04-15 09:19:24 +0500

    w

---
+++ index6.py
@@ -0,0 +1,18 @@
+# from datetime import datetime
+#
+# tugilgan_yil = input('Tug`ilgan yilingiz: ')
+#
+# bugun = datetime.today()
+#
+# natija = bugun.year - int(tugilgan_yil)
+#
+# print('Sizning yoshingiz ', natija)
+
+
+
+# import sys
+#
+# # print(sys.path)
+#
+# for p in sys.path:
+#     print(p)
